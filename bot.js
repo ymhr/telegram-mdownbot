4091f7b2888e085a330a9dd2029e712d3a17a1b4
jamesmartin <jamesmartin442@gmail.com>
2016-01-25 09:27:55 +0000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const config = require('./config');
+var TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(config.token, {polling: true});
+
+bot.on('inline_query', msg => {
+    const input = msg.query;
+    const queryId = msg.id;
+
+    console.log('Incoming request');
+
+    const response = {
+        id: 'res',
+        type: 'article',
+        parse_mode: 'markdown',
+        title: input,
+        message_text: input
+    };
+
+    if(input.length > 0){
+        bot.answerInlineQuery(queryId, [response], {})
+            .then(res => console.log(res));
+    }
+
+});
